6ead480dbca0046ef12b8ad27d7c731ad49bf548
Naumetsdmytro <113528509+Naumetsdmytro@users.noreply.github.com>
2023-01-26 21:50:06 +0000

    Deploying to gh-pages from @ Naumetsdmytro/goit-js-hw-09@d1b437711884fc3d5fcf99f4673b3e0e8903cda3 🚀

--- 02-timer.5a9f115f.js
+++
@@ -1,2 +0,0 @@
-!function(){function o(o){return String(o).padStart(2,"0")}var t=setInterval(e,1e3);function e(){var e,n,a,r,l=Date.now(),f=new Date(2023,0,26,13,6)-l;f<0?clearInterval(t):console.log((e=f,n=6e4,r=24*(a=36e5),{days:o(Math.floor(e/r)),hours:o(Math.floor(e%r/a)),minutes:o(Math.floor(e%r%a/n)),seconds:o(Math.floor(e%r%a%n/1e3))}))}e()}();
-//# sourceMappingURL=02-timer.5a9f115f.js.map
